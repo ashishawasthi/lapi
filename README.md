d7c97abe2f450998e76c0be1e0547ca156555dcb
ashishawasthi <ashish.awasthi@gmail.com>
2023-04-23 21:46:49 +0800

    Template examples for all 4 steps

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 Langchain REST API, with FastAPI
 
 ## Install
-
 ```bash
 pip install -r requirements.txt
 ```
+Set OPENAI_API_KEY in .env
 
 ## Run
-
 ```bash
 uvicorn main:app --reload
+```
+
+## Test (TBD) 
+```bash
+pytest
+```
+
+## API
+### POST /epics/
+```bash
+curl -X POST "http://localhost:8000/epics/" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"title\":\"my title\",\"description\":\"my test description\"}"
 ```
--- main.py
+++ main.py
@@ -1,68 +1,89 @@
-import json
+from langchain.chat_models import ChatOpenAI # TODO replace with from langchain.chat_models import AzureChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from fastapi import FastAPI
-from langchain.chat_models import ChatOpenAI
-#from langchain.chat_models import AzureChatOpenAI
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+from pydantic import BaseModel
 from dotenv import load_dotenv
+import json
+import os
 
-load_dotenv() # load OPENAI_API_KEY from .env
+load_dotenv() # load OPENAI_API_KEY from .env 
 app = FastAPI()
 
-system = """You are a technical product manager and solution architect. You write detailed technical user stories for given product requirements.
+class Clarification(BaseModel):
+    question: str
+    answer: str | None = None
 
-The Platform used for the product is AWS cloud. Following are the AWS services that can be used in the product:
-- Arbitration-Layer (proprietary channel routing engine for push to outbound channels, REST APIs for inbound channels)
-- T3 (proprietary data-lake, similar to AWS Athena)
-- T3 -Feature-Mart (proprietary customer  model input data and metadata management)
-- Job-Server (proprietary platform for data processing and model inference, similar to AWS EMR)
-- Analytical-Cluster (proprietary data science work-bench, similar to AWS SageMaker)
-- Core Banking (On-prem Account and transaction management system)
-- Credit Card & Loans System (On-prem) 
-- Internet Banking Website 
-- Mobile Banking App 
-- P2P Payment App
-- API Gateway
-- Data Loader (for loading data from T3 to MariaDB)
-- CyberArk (for managing secrets)
-- Confluent Kafka (as real time event source)
-- MariaDB (as application database)
-- ReactJS (for UI)
-- AWS S3
-- AWS SageMaker (for AI model training and inference)
-- AWS EMR (for AI model training and inference)
-- AWS Lambda
+class Requirement(BaseModel):
+    # TODO add problem_id when state is stored in database
+    description: str
+    clarifications: list[Clarification]
 
-Mention the users as 'Business user', 'Customer', 'System Developer' and other users if relevant e.g. 'Data Scientist', 'Analyst', 'Compliance Officer', 'Site Reliability Engineer'
+class Epic(BaseModel):
+    # TODO add requirement_id when state is stored in database
+    title: str
+    description: str
+    clarifications: list[Clarification]
 
-Write detailed technical user stories in a JSON array with title, user, description and questions for each story.
-"""
-requirement1 = """Business Requirements:
-A self-service marketing rule-engine for business users to develop targeted marketing rules using AI model recommendations and customer data, including product-propensities, demographics, behavior, service usage, and transaction history. The platform ensures minimal spam and delivers messages via inbound and outbound channels. AI models provide product recommendations, predict propensities, optimize relevance, and align with revenue goals, while determining the best contact method, channels, timing, and content. Data is sourced from T3 for historical customer information and from Confluence Kafka for real-time customer events."""
-epics1 = '[{"title":"Marketing rules for targeted messages","description":"Create marketing rules based on AI model recommendations and customer data to deliver targeted messages via inbound and outbound channels. The rules should allow me to filter customers based on product-propensities, demographics, behavior, recent usage of services, and transaction history. The rules should also comply with regulatory requirements and prevent spamming customers. The system should provide a user-friendly interface to create, edit, and delete rules.","questions":["List of events to be available in the system.","Examples of complex conditions to be supported in rules. e.g. timeout between start and end events, event sequence.","List of spamming prevention rules."]},{"title":"Arbitration between inbound and outbound channels","description":"Create rules to arbitrate across all inbound and outbound channels. Inbound channels should include mobile apps, website and support calls. Outbound channels should include push-notes, email and SMS. The rules should allow me to specify the priority of channels and the conditions under which a channel should be used. The system should integrate with Arbitration-Layer and provide options to setup routing to one or multiple channels of Arbitration-Layer. Rules should be able to use a mix of AI model recommended touch points/channels, product/offer information and customer data to determine the best channel to contact a customer.","questions":["List of all inbound and outbound channels to support.","Types and examples of criteria to be supported"]},{"title":"Train AI models and monitor model performance over time","description":"Train AI models to provide product recommendations, product propensities and content personalization. The system should provide a way to make training data available to Analytical-Cluster or AWS SageMaker to train the models. The system should also provide a way to update the models with new data and retrain them periodically.","questions":["Training data in T3-Feature-Mart should include customer demographics, behavior, service usage logs including Adobe/Google Analytics and customer transaction history.","Training data should be accessible in Analytical-Cluster or AWS SageMaker for training the models.","List of Model monitoring metrics. Definition of each metric.","Threshold values for each model monitoring metric, to raise alerts."]},{"title":"Integrate AI models into the rule engine system","user":"System Developer","description":"Integrate AI models into the rule engine system to predict product-propensity, maximizing relevance of products to customers and bank\'s revenue expectations. The AI models should predict the best way to contact a customer, determining the optimal channels, timing, and content. The system should provide REST APIs to access the real-time AI models in rule engine. The system should also provide batch scoring option on Job-Server or AWS EMR, where scores across all customers need to be calculated at the same time.","questions":["List of AI models. Inputs and outputs of each AI model","List of APIs. Inputs and outputs of each API","List of batch scoring jobs. Inputs and outputs of each job"]},{"title":"Measure campaign effectiveness using control groups","user":"Business user","description":"Measure the effectiveness of campaigns using control groups. The system should provide a way to create control groups, assign them to campaigns and compare the results with the treatment groups. The system should generate reports with metrics such as conversion rate, revenue generated, and customer satisfaction.","questions":["List of metrics to be used for measuring campaign effectiveness.","Treatment group evaluation criteria for stratified sampling.","Types and examples of campaigns to be supported."]},{"title":"Handle high volumes and build fault-tolerant and resilient system","user":"System Developer","description":"Handle high volume and velocity of events and actions without compromising performance or reliability. The event handling should use scalable platforms like AWS Lambda and AWS SQS to process events asynchronously and scale up or down based on the load. The system should also use caching and load balancing for APIs to improve performance and reduce latency.","questions":["Maximum expected volume of events and actions.","Expected expect historical data to be loaded."]},{"title":"Secure and compliant system","user":"Compliance Officer","description":"Ensure that the system is secure and compliant with the bank\'s policies and regulations. The system should use CyberArk or AWS KMS to store keys to encrypt data at rest and in transit. The system should also use an APM like Splunk or AWS CloudTrail to monitor and audit system activity. The system should be regularly tested for vulnerabilities and compliance with the bank\'s policies and regulations.","questions":["List of compliance tests to be performed."]},{"title":"System monitoring dashboard","user":"Site Reliability Engineer","description":"Ensure that the system health is monitored and displayed on a live dashboard. There should be configurable thresholds for system metrics, to raise alarms and send notifications relevant SRE and development teams.","questions":["List of system metrics to be monitored.","List of alarms to be raised.","List of notifications to be sent."]}]'
+class Story(BaseModel):
+    # TODO add epic_id when state is stored in database
+    title: str
+    user: str
+    description: str
+    clarifications: list[Clarification]
+
+class Scenario(BaseModel):
+    # TODO add story_id when state is stored in database
+    title: str
+    description: str
 
 @app.get("/")
 async def root():
-    return {"message": "Usage: Call /epics with the requirements text in the post body"}
+    return {"usage": """
+    /requirements with the problem-statement text in the post body.
+    /epics with the requirements JSON in the post body.
+    /user-stories with the epics JSON in the post body.
+    /test-scenarios with the user-stories JSON in the post body.
+    """}
 
 # Generate epics for the given requirements using OpenAI ChatCompletion API
-@app.get("/epics/{requirements}")
-@app.post("/epics")
-def epics(requirements: str):
-    req = "Business Requirements:\n{requirements}".format(requirements=requirements)
-    chat = ChatOpenAI(temperature=0.5, max_tokens=2000)
+@app.post("/epics") # TODO change to PUT when state is stored in database
+def epics(requirements: list[Requirement]):
+
+    project_dir = "rule-engine" # TODO find closest project to the given requirements
+
+    # Load the prompt parts from files at startup
+    system2 = text_from_file(os.path.join("assets", "prompts", "2-system.txt"))
+    requirement21 = json_string_from_file(os.path.join("assets", "prompts", project_dir, "21-requirements-answers.json"))
+    epics22 = json_string_from_file(os.path.join("assets", "prompts", project_dir, "22-epics-questions.json"))
+    chat = ChatOpenAI(temperature=0, max_tokens=2048, request_timeout=140, max_retries=2)
     messages = [
-        SystemMessage(content=system),
-        HumanMessage(content=requirement1),
-        AIMessage(content=epics1),
-        HumanMessage(content=req),
+        SystemMessage(content=system2),
+        HumanMessage(content=requirement21),
+        AIMessage(content=epics22),
+        HumanMessage(content="Business Requirements:\n{requirements}".format(requirements=requirements)),
     ]
-    print("\nCalling OpenAI ChatCompletion API with messages: {messages}".format(messages=messages))
-    epics_str = chat(messages).content
-    print("\nReceived epics from OpenAI ChatCompletion API: {epics}".format(epics=epics_str))
-    epics_json = json.loads(epics_str)
-    print("\n{epics_length} epics received from OpenAI ChatCompletion API".format(epics_length=len(epics_json)))
+    print("\n\nCalling OpenAI ChatCompletion API with messages: {messages}".format(messages=messages))
+    response = chat(messages)
+    print("\n\nResponse:" + response)
+    try:
+        epics_json = json.loads(response.content)
+        print("\n\n{epics_length} epics received from OpenAI ChatCompletion API".format(epics_length=len(epics_json)))
+    except ValueError as e:
+        print("\n\nError parsing epics JSON from OpenAI ChatCompletion API response: {e}".format(e=e))
+        epics_json = []
+        # split response.content by new lines. for each non-empty line, make it title of an epic
+        for line in response.content.splitlines():
+            if(line and line.strip() != ""):
+                epics_json.append({"title": line})
     return epics_json
+
+def json_string_from_file(file_path: str) -> str:
+    with open(os.path.join(file_path), "r") as f:
+        raw = f.read()
+    # remove extra whitespaces and newlines
+    return json.dumps(json.loads(raw)) 
+
+def text_from_file(file_path: str) -> str:
+    with open(os.path.join(file_path), "r") as f:
+        raw = f.read()
+    return raw.strip()
