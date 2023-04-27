165dac4c6d78df9419b2d97bf0ec17c82788cc6e
Tee hui Tan <ZhiHui@Tees-MacBook-Pro-2.local>
2023-04-27 14:12:48 +0800

    modifications to resolve syntax_type_naming issues

--- models.py
+++ models.py
@@ -1,26 +1,27 @@
+from typing import List, Optional
 from pydantic import BaseModel
 
 class Clarification(BaseModel):
     question: str
-    answer: str | None = None
+    answer: Optional[str]
 
 class Requirement(BaseModel):
     # TODO add problem_id when state is stored in database
     description: str
-    clarifications: list[Clarification]
+    clarifications: List[Clarification]
 
 class Epic(BaseModel):
     # TODO add requirement_id when state is stored in database
     title: str
     description: str
-    clarifications: list[Clarification]
+    clarifications:List[Clarification]
 
 class Story(BaseModel):
     # TODO add epic_id when state is stored in database
     title: str
     user: str
     description: str
-    clarifications: list[Clarification]
+    clarifications: List[Clarification]
 
 class Scenario(BaseModel):
     # TODO add story_id when state is stored in database
