4c7ffbfd29c09d2edf84067cf5516b68b775f2d6
Ashish <ashish.awasthi@gmail.com>
2023-07-01 18:29:33 +0800

    renamed function

--- figma/extract.py
+++ figma/extract.py
@@ -1,19 +1,19 @@
 import json
 
 # recursive call to extract name and type from document JSON 
-def extract_name_type(data):
+def extract_summary(data):
     if isinstance(data, dict):
         new_dict = {}
         for key, value in data.items():
             if key in ['name', 'type']:
                 new_dict[key] = value
             elif key == 'document':
-                new_dict[key] = extract_name_type(value)
+                new_dict[key] = extract_summary(value)
             elif key == 'children':
-                new_dict[key] = [extract_name_type(child) for child in value]
+                new_dict[key] = [extract_summary(child) for child in value]
         return new_dict
     elif isinstance(data, list):
-        return [extract_name_type(item) for item in data]
+        return [extract_summary(item) for item in data]
     else:
         return data
 
@@ -21,9 +21,8 @@ def extract_name_type(data):
 with open('response.json', 'r') as infile:
     data = json.load(infile)
 
-extracted_data = extract_name_type(data)
+summary = extract_summary(data)
 
 # TODO replace with return statement
-with open('extract.json', 'w') as outfile:
-    json.dump(extracted_data, outfile, indent=2)
-    
+with open('summary.json', 'w') as outfile:
+    json.dump(summary, outfile, indent=2)
