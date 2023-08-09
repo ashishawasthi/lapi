0602b6b05f61196219ede9b44791484c73b0dec6
ashishawasthi <ashish.awasthi@gmail.com>
2023-08-09 23:35:10 +0800

    added api /requirement-v for vertex AI

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from langchain.chat_models import ChatOpenAI # TODO replace with from langchain.
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from google.cloud import aiplatform
+
 import os
 import json
+import vertexai
 
 from models import Clarification, Requirement, Epic, Story, Scenario
 from utils import text_from_file, json_string_from_file, extract_json_array
+from vertexai.language_models import TextGenerationModel
 
 load_dotenv() # load OPENAI_API_KEY from .env 
 app = FastAPI()
@@ -105,3 +109,30 @@ def scenarios(stories: List[Story]):
     response = chat(messages)
     print("\n\nResponse: \n{response}".format(response=response))
     return extract_json_array(response.content)
+
+
+# Generate detailed requirement for the given problem statement using Vertex AI
+@app.post("/requirement-v") # TODO change to PUT when state is stored in database
+def requirement(problem: str):
+
+    project_dir = "rule-engine" # TODO find closest project to the given requirement
+
+    # Load the prompt parts from files
+    system1 = text_from_file(os.path.join("assets", "prompts", "1-system.txt"))
+    problem11 = text_from_file(os.path.join("assets", "prompts", project_dir, "11-problem.txt"))
+    requirement12 = json_string_from_file(os.path.join("assets", "prompts", project_dir, "12-requirement-questions.json"))
+
+    model = TextGenerationModel.from_pretrained("text-bison@001")
+    parameters = {"temperature": 0.2, "max_output_tokens": 1024, "top_p": 0.8, "top_k": 40}
+    vertexai.init(project="generativai", location="us-central1")
+    prompt = """{system1}
+Problem Statement: {problem11}
+Project Requirement: {requirement12}
+
+Problem Statement: {problem}
+Project Requirement:
+""".format(system1=system1, problem11=problem11, requirement12=requirement12, problem=problem)
+    print("\n\nCalling VertexAI Text API with prompt: {prompt}".format(prompt=prompt))
+    response = model.predict(prompt, **parameters)
+    print("\n\nResponse: \n{response}".format(response=response))
+    return extract_json_array(response.text)
